2e7b574bd4d3939f0b736c34168db223e10f4e37
Aditya Gupta <adityagupta11219@gmail.com>
2023-07-29 00:23:33 +0530

    setup for social media app

---
+++ graphql/resolvers/user.js
@@ -0,0 +1,91 @@
+const User = require("../../Models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+const {
+	validateRegisterInput,
+	validateLoginInput,
+} = require("../../utils/validators");
+
+const generateToken = (user) => {
+	return jwt.sign(
+		{
+			id: user._id,
+			email: user.email,
+			username: user.email,
+		},
+		SECRET_KEY,
+		{ expiresIn: "1h" }
+	);
+};
+
+module.exports = {
+	Query: {},
+	Mutation: {
+		async reqister(
+			_,
+			{ reqisterInput: { username, email, password, confirmPassword } },
+			context
+		) {
+			const { valid, errors } = validateRegisterInput(
+				username,
+				email,
+				password,
+				confirmPassword
+			);
+			if (!valid) {
+				throw new UserInputError("Errors", { errors });
+			}
+
+			const user = await User.findOne({ username });
+			if (user) {
+				throw new UserInputError("Username is taken", {
+					errors: {
+						username: "This username is taken",
+					},
+				});
+			}
+
+			password = await bycrpt.hash(password, 17);
+
+			const newUser = await User.create({
+				email,
+				username,
+				password,
+				createdAt: new Date.toISOString(),
+			});
+			const token = generateToken(newUser);
+			return {
+				...newUser._doc,
+				id: newUser._id,
+				token,
+			};
+		},
+		async login(_, { username, password }) {
+			const { errors, valid } = validateLoginInput(username, password);
+
+			if (!valid) {
+				throw new UserInputError("Errors", { errors });
+			}
+
+			const user = await User.findOne({ username });
+			if (!user) {
+				errors.general = "User not found";
+				throw new UserInputError("User not found", { errors });
+			}
+
+			const match = await bycrpt.compare(password, user.password);
+			if (!match) {
+				errors.general = "Wrong Credentails";
+				throw new UserInputError("Wrong Credentails", { errors });
+			}
+
+			const token = generateToken(user);
+			return {
+				...newUser._doc,
+				id: newUser._id,
+				token,
+			};
+		},
+	},
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+mongoose
+	.connect(process.env.MONGOURI)
+	.then(() => {
+		return server.listen({ port: 5000 });
+	})
+	.then((res) => {
+		console.log(`Server is running at ${res.url}`);
+	});
